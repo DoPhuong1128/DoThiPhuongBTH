e98afef5bb75d93c0b437f79e57bcecad7fafb95
DoPhuong1128 <mameo4869@gmail.com>
2023-03-19 21:42:19 +0700

    1303

---
+++ BaiThucHanh1303/Models/Process/GiaiPhuongTrinh.cs
@@ -0,0 +1,14 @@
+namespace BaiThucHanh1303.Models.Process
+{
+    public class GiaiPhuongTrinh
+    {
+        //xay dung cac phuong thuc
+        //phuong thuc giai phuong trinh bac 1
+        public string GiaiPhuongTrinhB1( double a, double b)
+        {
+            //code giai phuong trinh
+            double x = -b/a;
+            return " Ngiem cua phuong trinh X=" +x;
+        } 
+    }
+}
